0abffffc7d69b3996ca205f7e05731c621103736
Jorge Isaac Moncayo Loaiza <Jorge.moncayo.loaiza@udla.edu.ec>
2024-07-07 19:21:13 -0500

    listo

--- config/blacklist.js
+++ config/blacklist.js
@@ -1,5 +1,68 @@
-const listaNegraIPs = ["104.16.241.118", "76.76.21.142", "172.66.41.24"];
-const listaNegraDominios = ["example.com", "malicious.com"];
-const listaNegraUsuarios = ["baduser"];
+const listaNegraIPs = [
+    "104.16.241.118", 
+    "76.76.21.142", 
+    "172.66.41.24",
+    "192.168.1.100",
+    "203.0.113.5",
+    "198.51.100.23",
+    "203.0.113.200",
+    "198.51.100.150",
+    "172.16.254.1",
+    "45.33.32.156",
+    "144.202.14.38",
+    "107.170.232.60",
+    "173.255.226.98",
+    "103.21.244.0",
+    "103.22.200.0",
+    "103.31.4.0",
+    "104.16.0.0",
+    "104.24.0.0",
+    "108.162.192.0"
+];
+
+const listaNegraDominios = [
+    "malicious.com",
+    "phishing.com",
+    "spam.com",
+    "badwebsite.net",
+    "fakewebsite.org",
+    "scam-site.com",
+    "fraudsite.co",
+    "fake-login.com",
+    "harmfulwebsite.com",
+    "untrustedsite.net",
+    "dangerousdomain.org",
+    "malwaredomain.com",
+    "suspicioussite.com",
+    "maliciousdomain.net",
+    "badexample.com",
+    "examplephish.com",
+    "badactor.net",
+    "cyberattack.org",
+    "evilwebsite.com"
+];
+
+const listaNegraUsuarios = [
+    "baduser", 
+    "eviluser", 
+    "malicioususer",
+    "spammer",
+    "hacker123",
+    "frauduser",
+    "scammer",
+    "fakeaccount",
+    "badactor",
+    "malwareuser",
+    "phisher",
+    "botuser",
+    "suspicioususer",
+    "untrusteduser",
+    "dangeroususer",
+    "maliciousactor",
+    "spammer123",
+    "fakeuser",
+    "evilaccount",
+    "harmfuluser"
+];
 
 module.exports = { listaNegraIPs, listaNegraDominios, listaNegraUsuarios };
